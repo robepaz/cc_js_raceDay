ea6a2028f624df24797be6aa489f61dbabd68051
Roberto Paz <fsjedi@outlook.com>
2021-09-17 10:17:32 -0400

    agregamos primera condicion

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const registeredEarly = true;
 //agregamos edad de corredor
 const runnerAge = 22;
 
+if (runnerAge > 18 && registeredEarly) {
+    raceNumber += 1000;
+
+if (runnerAge > 18 && registeredEarly) {
+    console.log(`Racer ${raceNumber}, you will start off at 9:30`);
+
 let lateToWork = true;
 let oppositeValue = !lateToWork;
  
 console.log(oppositeValue); 
-// Prints: false
-
-// let lateToWork = true;
-// let oppositeValue = !lateToWork;
- 
-// console.log(oppositeValue); 
-// // Prints: false
