705b1ae3bc2f864523710bc629ed8d9d5e7e5064
Roberto Paz <fsjedi@outlook.com>
2021-09-17 10:03:16 -0400

    generamos aleatorio

---
+++ app.js
@@ -0,0 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const registeredEarly = true;
+
+// const isTaskCompleted = false;
+ 
+// if (isTaskCompleted) {
+//   console.log('Task completed');
+// } else {
+//   console.log('Task incomplete');
+// }
+
+// true && true;      // true
+// 1 > 2 && 2 > 1;    // false
+// true && false;     // false
+// 4 === 4 && 3 > 1;  // true
