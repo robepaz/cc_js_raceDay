368337c5a4486a8beed4c3ab7344649746d2bc1c
Roberto Paz <fsjedi@outlook.com>
2021-09-17 10:22:16 -0400

    antes de break

--- app.js
+++ app.js
@@ -11,7 +11,31 @@ if (runnerAge > 18 && registeredEarly) {
 if (runnerAge > 18 && registeredEarly) {
     console.log(`Racer ${raceNumber}, you will start off at 9:30`);
 
-let lateToWork = true;
-let oppositeValue = !lateToWork;
- 
-console.log(oppositeValue); 
+    switch (randomNumber) {
+        // case 1:
+        //   console.log('It is certain')
+        //   break
+        // case 2:
+        //   console.log('It is decidedly so')
+        //   break
+        case 3:
+          console.log('Reply hazy try again')
+          break
+        case 4:
+          console.log('Cannot predict now')
+          break
+        case 5:
+          console.log('Do not count on it')
+          break
+        case 6:
+          console.log('My sources say no')
+          break
+        case 7:
+          console.log('Signs point to yes')
+          break
+        default:
+          console.log('Reply hazy try again')
+          break
+      }
+      
+
