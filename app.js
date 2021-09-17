03671ca4703e3c1274f6249f300bdbe2f510e4f5
Roberto Paz <fsjedi@outlook.com>
2021-09-17 10:33:31 -0400

    final

--- app.js
+++ app.js
@@ -8,30 +8,16 @@ const runnerAge = 22;
 if (runnerAge > 18 && registeredEarly) {
     raceNumber += 1000;
 
+// adultos tempraneros
 if (runnerAge > 18 && registeredEarly) {
     console.log(`Racer ${raceNumber}, you will start off at 9:30`);
 
+// adultos tardios
 } else if (runnerAge > 18 && !registeredEarly) {
     console.log(`Racer ${raceNumber}, you will start off at 11:00`);
 
-
-    switch (randomNumber) {
-        
-        case 4:
-          console.log('Cannot predict now')
-          break
-        case 5:
-          console.log('Do not count on it')
-          break
-        case 6:
-          console.log('My sources say no')
-          break
-        case 7:
-          console.log('Signs point to yes')
-          break
-        default:
-          console.log('Reply hazy try again')
-          break
-      }
-      
+// menores cualquier edad    
+} else {
+    console.log(`Racer ${raceNumber}, you will start off at 12:30`);
+  }
 
